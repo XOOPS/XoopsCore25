4eb577c5d072b6cab584f39af8262057dbfc0558
geekwright <richard@geekwright.com>
2016-03-08 12:58:44 -0600

    Update copyright date

--- upgrade/upd-2.4.0-to-2.4.1/license.dist.php
+++ upgrade/upd-2.4.0-to-2.4.1/license.dist.php
@@ -12,7 +12,7 @@
 /**
  *  Xoops Version
  *
- * @copyright       (c) 2000-2015 XOOPS Project (www.xoops.org)
+ * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
  * @license             GNU GPL 2 (http://www.gnu.org/licenses/gpl-2.0.html)
  * @package             kernel
  * @version             $Id: license.dist.php 8066 2011-11-06 05:09:33Z beckmi $
