6974e64fbe7927cb9a8f44d618c654bfe6613c0f
Txmod Xoops <txmodxoops@gmail.com>
2016-05-19 00:00:54 +0200

    New Class XoopsFormTime
    Example Integration:
    
    $form->addElement(new XoopsFormTime( _TIME, 'time', 20, date('h:i:s')));

---
+++ xoopsformloader.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * XOOPS form class loader
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
+ * @license             GNU GPL 2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package             kernel
+ * @since               2.0.0
+ */
+
+xoops_load('XoopsForm');
+xoops_load('XoopsThemeForm');
+xoops_load('XoopsSimpleForm');
+xoops_load('XoopsFormElement');
+xoops_load('XoopsFormElementTray');
+xoops_load('XoopsFormLabel');
+xoops_load('XoopsFormCheckBox');
+xoops_load('XoopsFormPassword');
+xoops_load('XoopsFormButton');
+xoops_load('XoopsFormButtonTray'); // To be cleaned
+xoops_load('XoopsFormHidden');
+xoops_load('XoopsFormFile');
+xoops_load('XoopsFormRadio');
+xoops_load('XoopsFormRadioYN');
+xoops_load('XoopsFormSelect');
+xoops_load('XoopsFormSelectGroup');
+xoops_load('XoopsFormSelectCheckGroup'); // To be cleaned
+xoops_load('XoopsFormSelectUser');
+xoops_load('XoopsFormSelectTheme');
+xoops_load('XoopsFormSelectMatchOption');
+xoops_load('XoopsFormSelectCountry');
+xoops_load('XoopsFormSelectTimeZone');
+xoops_load('XoopsFormSelectLang');
+xoops_load('XoopsFormSelectEditor');
+xoops_load('XoopsFormText');
+xoops_load('XoopsFormTextArea');
+xoops_load('XoopsFormTextDateSelect');
+xoops_load('XoopsFormDhtmlTextArea');
+xoops_load('XoopsFormTime');
+xoops_load('XoopsFormDateTime');
+xoops_load('XoopsFormHiddenToken');
+xoops_load('XoopsFormColorPicker');
+xoops_load('XoopsFormCaptcha');
+xoops_load('XoopsFormEditor');
