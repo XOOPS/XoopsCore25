5f43f9aeb6e088fb87b6484a953159b5cd2dfb47
geekwright <richard@geekwright.com>
2016-08-17 16:59:26 -0500

    Shift toward HTML5

--- htdocs/install/include/install_tpl.php
+++ htdocs/install/include/install_tpl.php
@@ -29,20 +29,20 @@
 defined('XOOPS_INSTALL') or die('XOOPS Installation wizard die');
 include_once '../language/' . $wizard->language . '/global.php';
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo _LANGCODE; ?>" lang="<?php echo _LANGCODE; ?>">
-
+<!doctype html>
+<html>
 <head>
+    <meta charset="UTF-8">
     <title>
         <?php echo XOOPS_VERSION . ' : ' . XOOPS_INSTALL_WIZARD; ?>
         (<?php echo ($wizard->pageIndex + 1) . '/' . count($wizard->pages); ?>)
     </title>
     <meta http-equiv="Content-Type" content="text/html; charset=<?php echo _INSTALL_CHARSET ?>"/>
     <link rel="shortcut icon" type="image/ico" href="../favicon.ico"/>
-    <link charset="UTF-8" rel="stylesheet" type="text/css" media="all" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" media="all" href="css/style.css"/>
     <?php
     if (file_exists('language/' . $wizard->language . '/style.css')) {
-        echo '<link charset="UTF-8" rel="stylesheet" type="text/css" media="all" href="language/' . $wizard->language . '/style.css" />';
+        echo '<link rel="stylesheet" type="text/css" media="all" href="language/' . $wizard->language . '/style.css" />';
     }
     ?>
 
