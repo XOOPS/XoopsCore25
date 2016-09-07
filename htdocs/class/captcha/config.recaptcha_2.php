7d327e11506e6766ed0e96c7480e6ab4fd94f5dc
geekwright <richard@geekwright.com>
2016-09-07 17:46:34 -0500

    UTF-8 conversion

--- htdocs/class/captcha/config.recaptcha_2.php
+++ htdocs/class/captcha/config.recaptcha_2.php
@@ -8,13 +8,13 @@
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
- 
+
 /**
  * CAPTCHA configurations for Recaptcha 2 mode
  *
  * @package     class
  * @subpackage  CAPTCHA
- * @author      Grégory Mage
+ * @author      GrÃ©gory Mage
  * @copyright   2016 XOOPS Project (http://xoops.org)
  * @license     GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
  * @link        http://xoops.org
@@ -23,4 +23,5 @@ defined('XOOPS_ROOT_PATH') || exit('Restricted access');
 
 return $config = array(
     'website_key' => 'YourWebsiteKey', //https://www.google.com/recaptcha/intro/index.html YourWebsiteKey
-    'secret_key'  => 'YourSecretKey',); //https://www.google.com/recaptcha/intro/index.html YourSecretKey
+    'secret_key'  => 'YourSecretKey',  //https://www.google.com/recaptcha/intro/index.html YourSecretKey
+);
