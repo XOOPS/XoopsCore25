a3692185d4ec5dc96807bbf7e975d1480afdf50a
geekwright <richard@geekwright.com>
2017-02-20 09:22:31 -0600

    Reset actkey on activation
    Prevent reuse of an activation key by a deactivated user.

--- htdocs/modules/profile/admin/deactivate.php
+++ htdocs/modules/profile/admin/deactivate.php
@@ -2,12 +2,13 @@
 include_once __DIR__ . '/admin_header.php';
 xoops_cp_header();
 
-if (!isset($_REQUEST['uid'])) {
+$uid = Xmf\Request::getInt('uid', 0);
+if ($uid === 0) {
     redirect_header('index.php', 2, _PROFILE_AM_NOSELECTION);
 }
 /* @var $member_handler XoopsMemberHandler */
 $member_handler = xoops_getHandler('member');
-$user           = $member_handler->getUser($_REQUEST['uid']);
+$user           = $member_handler->getUser($uid);
 if (!$user || $user->isNew()) {
     redirect_header('index.php', 2, _PROFILE_AM_USERDONEXIT);
 }
@@ -15,15 +16,25 @@ if (!$user || $user->isNew()) {
 if (in_array(XOOPS_GROUP_ADMIN, $user->getGroups())) {
     redirect_header('index.php', 2, _PROFILE_AM_CANNOTDEACTIVATEWEBMASTERS);
 }
-$user->setVar('level', $_REQUEST['level']);
-if ($member_handler->insertUser($user)) {
-    if ($_REQUEST['level'] == 1) {
+$level = Xmf\Request::getInt('level', 0);
+if ($level===0) {
+    $user->setVar('level', 0);
+    // reset the activation key so it cannot be reused
+    // this now gets done at activation, but we do it here also to fix accounts created before the change.
+    $actkey = substr(md5(uniqid(mt_rand(), 1)), 0, 8);
+    $user->setVar('actkey', $actkey);
+    $result = $member_handler->insertUser($user);
+} else {
+    $result = $member_handler->activateUser($user);
+}
+if ($result) {
+    if ($_REQUEST['level'] !== 0) {
         $message = _PROFILE_AM_USER_ACTIVATED;
     } else {
         $message = _PROFILE_AM_USER_DEACTIVATED;
     }
 } else {
-    if ($_REQUEST['level'] == 1) {
+    if ($_REQUEST['level'] !== 0) {
         $message = _PROFILE_AM_USER_NOT_ACTIVATED;
     } else {
         $message = _PROFILE_AM_USER_NOT_DEACTIVATED;
