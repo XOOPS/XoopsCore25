62365a90c17c8d2d6e22d69ae3de333497c927ce
geekwright <richard@geekwright.com>
2017-02-21 15:25:59 -0600

    Update bootstrap.min.css

--- htdocs/install/include/install_tpl.php
+++ htdocs/install/include/install_tpl.php
@@ -43,7 +43,7 @@ include_once '../language/' . $wizard->language . '/global.php';
         (<?php echo ($wizard->pageIndex + 1) . '/' . count($wizard->pages); ?>)</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="assets/css/style.css" rel="stylesheet">
