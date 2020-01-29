a512430fd9364fde4f3d0cbb58e975bf5e150b62
geekwright <richard@geekwright.com>
2020-01-28 21:19:03 -0600

    Update class docblock

--- htdocs/class/xoopsform/renderer/XoopsFormRendererBootstrap4.php
+++ htdocs/class/xoopsform/renderer/XoopsFormRendererBootstrap4.php
@@ -14,9 +14,8 @@
  * @category  XoopsForm
  * @package   XoopsFormRendererBootstrap4
  * @author    Richard Griffith <richard@geekwright.com>
- * @copyright 2018 XOOPS Project (http://xoops.org)
- * @license   GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
- * @link      http://xoops.org
+ * @copyright 2018-2020 XOOPS Project (https://xoops.org)
+ * @license   GNU GPL 2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
  */
 class XoopsFormRendererBootstrap4 implements XoopsFormRendererInterface
 {
