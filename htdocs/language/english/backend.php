0275576c7587dc250468edfb400a3ab5a1a392cf
geekwright <richard@geekwright.com>
2020-02-26 22:38:54 -0600

    Tweak feeds used in XOOPS News in admin.php
    - use https and no "www." for xoops.org feeds
    - give priority to feed(s) in htdocs/language/.../backend.php
    - present multiple feeds sequentially, not merged
    
    Re: #782

--- htdocs/language/english/backend.php
+++ htdocs/language/english/backend.php
@@ -1,8 +1,8 @@
 <?php
-// 
+//
 // _LANGCODE: en
 // _CHARSET : UTF-8
 // Translator: XOOPS Translation Team
 
 // RSS feed URLs
-return array('http://www.xoops.org/backend.php');
+return array('https://xoops.org/modules/publisher/backend.php');
