e577da9ac3e6cb2b15a374952666c7de069cd941
liomj <liomj83@gmail.com>
2020-05-05 00:36:27 +0800

    Fix Profile Index Template
    https://github.com/XOOPS/XoopsCore25/issues/559

--- htdocs/modules/profile/index.php
+++ htdocs/modules/profile/index.php
@@ -30,7 +30,7 @@ if (isset($_POST['op'])) {
 
 if ($op === 'main') {
     if (!is_object($GLOBALS['xoopsUser'])) {
-        $GLOBALS['xoopsOption']['template_main'] = 'system_userform.tpl';
+        $GLOBALS['xoopsOption']['template_main'] = 'profile_userform.tpl';
         include $GLOBALS['xoops']->path('header.php');
         $GLOBALS['xoopsTpl']->assign('lang_login', _LOGIN);
         $GLOBALS['xoopsTpl']->assign('lang_username', _USERNAME);
