cab9a1de6bff3be9b2531d2b0e246b039d04e66f
mambax7 <mambax7@gmail.com>
2021-08-27 10:37:04 -0400

    Undefined variable: $this->db

--- htdocs/modules/system/include/update.php
+++ htdocs/modules/system/include/update.php
@@ -67,7 +67,7 @@ function update_system_v211($module)
     }
     $sql = 'SHOW INDEX FROM ' . $xoopsDB->prefix('tplfile') . " WHERE KEY_NAME = 'tpl_refid_module_set_file_type'";
     if (!$result = $xoopsDB->queryF($sql)) {
-        xoops_error($this->db->error() . '<br>' . $sql);
+        xoops_error($xoopsDB->error() . '<br>' . $sql);
 
         return false;
     }
