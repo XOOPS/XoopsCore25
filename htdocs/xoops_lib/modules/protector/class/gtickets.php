01b96517c89bf4c5e089dcf0423190a79f1cfa4b
GregMage <gregory.mage@gmail.com>
2022-01-21 10:40:17 +0100

    Elimination of version_compare to keep the code cleaner.

--- htdocs/xoops_lib/modules/protector/class/gtickets.php
+++ htdocs/xoops_lib/modules/protector/class/gtickets.php
@@ -119,11 +119,7 @@ if (!class_exists('XoopsGTicket')) {
             global $xoopsModule;
 
             if ('' === $salt) {
-				if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
-					$salt = '$2y$07$' . str_replace('+', '.', base64_encode(random_bytes(16)));
-				} else {
-					$salt = '$2y$07$' . str_replace('+', '.', base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)));
-				}
+				$salt = '$2y$07$' . str_replace('+', '.', base64_encode(random_bytes(16)));
             }
 
             // create a token
