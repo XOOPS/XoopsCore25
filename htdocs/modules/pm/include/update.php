0073ce6e0db03def4c1cecbdfe8c74bb017d7968
mambax7 <mambax7@gmail.com>
2023-03-07 02:51:53 -0500

    add missing fields in PM

--- htdocs/modules/pm/include/update.php
+++ htdocs/modules/pm/include/update.php
@@ -57,6 +57,15 @@ function xoops_module_update_pm(XoopsModule $module, $oldversion = null)
             if ($fileinfo->getExtension() === 'html' && $fileinfo->getFilename() !== 'index.html') {
                 @unlink($templateDirectory . $v);
             }
+
+            // add to_delete fields
+            $sql = 'ALTER TABLE ' . $GLOBALS['xoopsDB']->prefix('priv_msgs')
+                   . " ADD `from_delete` tinyint(1) unsigned     NOT NULL default '1',
+  ADD `from_save`   tinyint(1) unsigned     NOT NULL default '0',
+  ADD `to_delete`   tinyint(1) unsigned     NOT NULL default '0',
+  ADD `to_save`     tinyint(1) unsigned     NOT NULL default '0',  
+  ADD INDEX `prune` (`msg_time`, `read_msg`, `from_save`, `to_delete`)";
+            $GLOBALS['xoopsDB']->queryF($sql);
         }
 
         xoops_load('xoopsfile');
