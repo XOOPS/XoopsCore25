dac03b8ce9c7afdb9e1af2b15eb7974c6d729a7c
mambax7 <mambax7@gmail.com>
2023-10-19 23:52:57 -0400

    The variable $rev_ip does not seem to be defined for all execution paths leading up to this point.

--- htdocs/xoops_lib/modules/protector/filters_disabled/postcommon_post_deny_by_httpbl.php
+++ htdocs/xoops_lib/modules/protector/filters_disabled/postcommon_post_deny_by_httpbl.php
@@ -19,6 +19,10 @@ class Protector_postcommon_post_deny_by_httpbl extends ProtectorFilterAbstract
 
         global $xoopsUser;
 
+        // Initialize $rev_ip to a default value.
+        $rev_ip = '';
+
+
         if (isset($_SERVER['REMOTE_ADDR'])) {
             $rev_ip = implode('.', array_reverse(explode('.', $_SERVER['REMOTE_ADDR'])));
         }
