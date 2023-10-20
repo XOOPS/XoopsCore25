a2a8ab7e8eeb91559b53d0edd77687d432106bdc
mambax7 <mambax7@gmail.com>
2023-10-20 11:54:12 -0400

    Revert "Reverting footer.php"
    This reverts commit e9c1f85799054cb24fd228aa2a76aaa7d1e02ac1.

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -25,30 +25,45 @@ if (!defined('XOOPS_FOOTER_INCLUDED')) {
 
     $xoopsLogger = XoopsLogger::getInstance();
     $xoopsLogger->stopTime('Module display');
-	// RMV-NOTIFY
-	include_once $GLOBALS['xoops']->path('include/notification_select.php');
-	if (!headers_sent()) {
-		header('Content-Type:text/html; charset=' . _CHARSET);
-		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-		//header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-		header('Cache-Control: private, no-cache');
-		header('Pragma: no-cache');
-	}
 
-	//@internal: using global $xoTheme dereferences the variable in old versions, this does not
-	if (!isset($xoTheme)) {
-		$xoTheme = $GLOBALS['xoTheme'];
-	}
+    if ($xoopsOption['theme_use_smarty'] == 0) {
+        // the old way
+        $footer = htmlspecialchars($xoopsConfigMetaFooter['footer']) . '<br><div class="txtcenter small">Powered by XOOPS &copy; <a href="http://xoops.org" rel="external" title="The XOOPS Project">The XOOPS Project</a></div>';
+        if (isset($GLOBALS['xoopsOption']['template_main'])) {
+            $xoopsTpl->caching = 0;
+            $xoopsTpl->display('db:' . $GLOBALS['xoopsOption']['template_main']);
+        }
+        if (!isset($xoopsOption['show_rblock'])) {
+            $xoopsOption['show_rblock'] = 0;
+        }
+        themefooter($xoopsOption['show_rblock'], $footer);
+        xoops_footer();
+    } else {
+        // RMV-NOTIFY
+        include_once $GLOBALS['xoops']->path('include/notification_select.php');
+        if (!headers_sent()) {
+            header('Content-Type:text/html; charset=' . _CHARSET);
+            header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+            //header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+            header('Cache-Control: private, no-cache');
+            header('Pragma: no-cache');
+        }
 
-	if (isset($GLOBALS['xoopsOption']['template_main']) && $GLOBALS['xoopsOption']['template_main'] != $xoTheme->contentTemplate) {
-		trigger_error("xoopsOption['template_main'] should be defined before including header.php", E_USER_WARNING);
-		if (false === strpos($GLOBALS['xoopsOption']['template_main'], ':')) {
-			$xoTheme->contentTemplate = 'db:' . $GLOBALS['xoopsOption']['template_main'];
-		} else {
-			$xoTheme->contentTemplate = $GLOBALS['xoopsOption']['template_main'];
-		}
-	}
-	$xoTheme->render();
+        //@internal: using global $xoTheme dereferences the variable in old versions, this does not
+        if (!isset($xoTheme)) {
+            $xoTheme = $GLOBALS['xoTheme'];
+        }
+
+        if (isset($GLOBALS['xoopsOption']['template_main']) && $GLOBALS['xoopsOption']['template_main'] != $xoTheme->contentTemplate) {
+            trigger_error("xoopsOption['template_main'] should be defined before including header.php", E_USER_WARNING);
+            if (false === strpos($GLOBALS['xoopsOption']['template_main'], ':')) {
+                $xoTheme->contentTemplate = 'db:' . $GLOBALS['xoopsOption']['template_main'];
+            } else {
+                $xoTheme->contentTemplate = $GLOBALS['xoopsOption']['template_main'];
+            }
+        }
+
+        $xoTheme->render();
     $xoopsLogger->stopTime();
 }
 
