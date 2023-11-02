01b54e32dc327d28fe5d893408b9cd66e03ed86c
mambax7 <mambax7@gmail.com>
2023-11-02 05:49:58 -0400

    delete duplicate closedir($handler);

--- htdocs/xoops_lib/modules/protector/oninstall.php
+++ htdocs/xoops_lib/modules/protector/oninstall.php
@@ -74,46 +74,45 @@ if (!function_exists('protector_oninstall_base')) {
 
         // TEMPLATES
         $tplfile_handler = xoops_getHandler('tplfile');
-        $tpl_path        = __DIR__ . '/templates';
+        $tpl_path = __DIR__ . '/templates';
         // Check if the directory exists
         if (is_dir($tpl_path)) {
             // Try to open the directory
             $handler = opendir($tpl_path . '/');
             if (false !== $handler) {
-            while (($file = readdir($handler)) !== false) {
-                if (substr($file, 0, 1) === '.') {
-                    continue;
-                }
-                $file_path = $tpl_path . '/' . $file;
-                if (is_file($file_path) && in_array(strrchr($file, '.'), array('.html', '.css', '.js'))) {
-                    $mtime   = (int)(@filemtime($file_path));
-                    $tplfile = $tplfile_handler->create();
-                    $tplfile->setVar('tpl_source', file_get_contents($file_path), true);
-                    $tplfile->setVar('tpl_refid', $mid);
-                    $tplfile->setVar('tpl_tplset', 'default');
-                    $tplfile->setVar('tpl_file', $mydirname . '_' . $file);
-                    $tplfile->setVar('tpl_desc', '', true);
-                    $tplfile->setVar('tpl_module', $mydirname);
-                    $tplfile->setVar('tpl_lastmodified', $mtime);
-                    $tplfile->setVar('tpl_lastimported', 0);
-                    $tplfile->setVar('tpl_type', 'module');
-                    if (!$tplfile_handler->insert($tplfile)) {
-                        $ret[] = '<span style="color:#ff0000;">ERROR: Could not insert template <b>' . htmlspecialchars($mydirname . '_' . $file, ENT_QUOTES) . '</b> to the database.</span><br>';
-                    } else {
-                        $tplid = $tplfile->getVar('tpl_id');
-                        $ret[] = 'Template <b>' . htmlspecialchars($mydirname . '_' . $file, ENT_QUOTES) . '</b> added to the database. (ID: <b>' . $tplid . '</b>)<br>';
-                        // generate compiled file
-                        include_once XOOPS_ROOT_PATH . '/class/xoopsblock.php';
-                        include_once XOOPS_ROOT_PATH . '/class/template.php';
-                        if (!xoops_template_touch($tplid)) {
-                            $ret[] = '<span style="color:#ff0000;">ERROR: Failed compiling template <b>' . htmlspecialchars($mydirname . '_' . $file, ENT_QUOTES) . '</b>.</span><br>';
+                while (($file = readdir($handler)) !== false) {
+                    if (substr($file, 0, 1) === '.') {
+                        continue;
+                    }
+                    $file_path = $tpl_path . '/' . $file;
+                    if (is_file($file_path) && in_array(strrchr($file, '.'), array('.html', '.css', '.js'))) {
+                        $mtime = (int)(@filemtime($file_path));
+                        $tplfile = $tplfile_handler->create();
+                        $tplfile->setVar('tpl_source', file_get_contents($file_path), true);
+                        $tplfile->setVar('tpl_refid', $mid);
+                        $tplfile->setVar('tpl_tplset', 'default');
+                        $tplfile->setVar('tpl_file', $mydirname . '_' . $file);
+                        $tplfile->setVar('tpl_desc', '', true);
+                        $tplfile->setVar('tpl_module', $mydirname);
+                        $tplfile->setVar('tpl_lastmodified', $mtime);
+                        $tplfile->setVar('tpl_lastimported', 0);
+                        $tplfile->setVar('tpl_type', 'module');
+                        if (!$tplfile_handler->insert($tplfile)) {
+                            $ret[] = '<span style="color:#ff0000;">ERROR: Could not insert template <b>' . htmlspecialchars($mydirname . '_' . $file, ENT_QUOTES) . '</b> to the database.</span><br>';
                         } else {
-                            $ret[] = 'Template <b>' . htmlspecialchars($mydirname . '_' . $file, ENT_QUOTES) . '</b> compiled.</span><br>';
+                            $tplid = $tplfile->getVar('tpl_id');
+                            $ret[] = 'Template <b>' . htmlspecialchars($mydirname . '_' . $file, ENT_QUOTES) . '</b> added to the database. (ID: <b>' . $tplid . '</b>)<br>';
+                            // generate compiled file
+                            include_once XOOPS_ROOT_PATH . '/class/xoopsblock.php';
+                            include_once XOOPS_ROOT_PATH . '/class/template.php';
+                            if (!xoops_template_touch($tplid)) {
+                                $ret[] = '<span style="color:#ff0000;">ERROR: Failed compiling template <b>' . htmlspecialchars($mydirname . '_' . $file, ENT_QUOTES) . '</b>.</span><br>';
+                            } else {
+                                $ret[] = 'Template <b>' . htmlspecialchars($mydirname . '_' . $file, ENT_QUOTES) . '</b> compiled.</span><br>';
+                            }
                         }
                     }
                 }
-            }
-            closedir($handler);
             } else {
                 // Handle the error condition when opendir fails
                 $ret[] = '<span style="color:#ff0000;">ERROR: Could not open the directory:  <b>' . htmlspecialchars($tpl_path) . '</b>.</span><br>';
