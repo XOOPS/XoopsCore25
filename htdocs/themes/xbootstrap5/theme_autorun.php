edb5da391e28b4bc37e02681ac6c1b1ba6e49a17
Michael Beck <mambax7@gmail.com>
2025-08-18 10:02:52 -0400

    xbootstrap5 and templates

--- htdocs/themes/xbootstrap5/theme_autorun.php
+++ htdocs/themes/xbootstrap5/theme_autorun.php
@@ -3,10 +3,18 @@
 xoops_load('XoopsFormRendererBootstrap5');
 XoopsFormRenderer::getInstance()->set(new XoopsFormRendererBootstrap5());
 
-global $xoopsTpl;
+global $xoopsTpl, $xoTheme;
+if (!empty($xoopsTpl)) {
+	$xoopsTpl->addConfigDir(__DIR__);
+}
+
+if (null === $xoopsTpl){
+	require_once XOOPS_ROOT_PATH . '/class/template.php';
+	$xoopsTpl = new \XoopsTpl();
+}
+
 $iconOverrides = include __DIR__ . '/config/module_icons.php';
 
 if (null !== $xoopsTpl) {
     $xoopsTpl->assign('iconOverrides', $iconOverrides);
 }
-
