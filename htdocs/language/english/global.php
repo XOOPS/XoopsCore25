3ba5ffd094c534b84e737f14473de07cd70f67e6
Michael Beck <mambax7@gmail.com>
2026-02-19 02:55:34 -0500

    add _TEXT_DIRECTION and _RTL

--- htdocs/language/english/global.php
+++ htdocs/language/english/global.php
@@ -264,3 +264,14 @@ define('_PRINT', 'Print');
 define('_PDF', 'PDF');
 define('_OFF', 'Off');
 define('_DB_QUERY_ERROR', 'Query Failed! SQL: %s - Error: ');
+
+//XOOPS 2.5.12
+// Canonical string (what templates and helpers should use)
+if (!defined('_TEXT_DIRECTION')) {
+    define('_TEXT_DIRECTION', 'ltr'); // or 'rtl'
+}
+
+// Convenience / legacy compat
+if (!defined('_RTL')) {
+    define('_RTL', _TEXT_DIRECTION === 'rtl');
+}
